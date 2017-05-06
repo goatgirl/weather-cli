4db7e91950b7d85ae3eee535f13d20e4c037dbcc
Spiral Wood <musictoon101@gmail.com>
2017-05-06 22:55:35 +0100

    Add call to weather api

---
+++ example.settings.py
@@ -0,0 +1,4 @@
+weather = dict(
+    url='https://api.darksky.net/forecast/{key}/{lat},{lng}',
+    key='<darksky-api-key>'
+)
--- main.py
+++ main.py
@@ -3,15 +3,36 @@
 import requests
 from urllib.parse import quote
 
+import settings
+
 
 def geocode(location):
     url = 'https://maps.googleapis.com/maps/api/geocode/json?address={}'
     enc_url = url.format(quote(location))
-    return requests.get(url=enc_url).json()
+    return requests.get(enc_url).json()
 
 
-data = geocode('huddersfield west yorkshire')
+def weather(latitude, longitude):
+    url = settings.weather['url'].format(
+        key=settings.weather['key'],
+        lat=latitude,
+        lng=longitude
+    )
+    return requests.get(url).json()
 
-print('Address:', data['results'][0]['formatted_address'])
-print('latitude:', data['results'][0]['geometry']['location']['lat'])
-print('longitude:', data['results'][0]['geometry']['location']['lng'])
+
+def f2c(f):
+    return int((f - 32) * 5 / 9)
+
+
+geo = geocode('hebdon bridge west yorkshire')
+
+lat = geo['results'][0]['geometry']['location']['lat']
+lng = geo['results'][0]['geometry']['location']['lng']
+
+data = weather(lat, lng)
+
+print('Address:', geo['results'][0]['formatted_address'])
+print('Summary:', data['currently']['summary'])
+print('Temperature: {}\xb0C'.format(f2c(data['currently']['temperature'])))
+print('Feels like: {}\xb0C'.format(f2c(data['currently']['apparentTemperature'])))
