a5a4fd53ca1b1af93308f57b0ff350232b6e70a0
Spiral Wood <musictoon101@gmail.com>
2017-05-07 03:55:55 +0100

    Add arg parser and default  locations

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import argparse
 import requests
 from urllib.parse import quote
 from settings import config
@@ -12,7 +13,6 @@ def geocode(location):
             print('Address not found')
         else:
             return res['results'][0]
-
     except Exception:
         print('Unable to connect to server')
 
@@ -23,27 +23,75 @@ def weather(latitude, longitude):
         lat=latitude,
         lng=longitude
     )
-
     return requests.get(url).json()
 
 
-def celcius(f):
-    return int((f - 32) * 5 / 9)
+def parse_address(address):
+    data = ''
+    for word in address:
+        data += ' ' + word
+    return data.strip()
+
+
+def set_default(location):
+    with open("weather.sav", 'w') as f:
+        f.write(str(location))
+
+
+def get_default():
+    with open("weather.sav", 'r') as f:
+        loc = f.read()
+    return loc
 
 
 def main():
-    geo = geocode('90210')
+    parser = argparse.ArgumentParser()
+    parser.add_argument("address",
+                        help="The address, city or post code for the weather.",
+                        nargs='*', type=str)
+
+    parser.add_argument("-d", "--default",
+                        help="Sets the  location as the default location and exits",
+                        action="store_true")
+
+    parser.add_argument("-s", "--set",
+                        help="Sets the  location as the default location and fetches weather",
+                        action="store_true")
+
+    args = parser.parse_args()
+
+    if args.address:
+        address = parse_address(args.address)
+    else:
+        address = get_default()
+
+    if not address:
+        print("No default location set.\nPlease run again and include an address.")
+        return
+
+    geo = geocode(address)
     if not geo:
         return
+
+    if args.default:
+        set_default(geo['formatted_address'])
+        print('Setting {} as default location'.format(geo['formatted_address']))
+        return
+
+    if args.set:
+        set_default(geo['formatted_address'])
+        print('Setting {} as default location'.format(geo['formatted_address']))
 
     lat = geo['geometry']['location']['lat']
     lng = geo['geometry']['location']['lng']
 
     data = weather(lat, lng)
 
-    print('Address:', geo['formatted_address'])
-    print('Weather:', data['currently']['summary'])
-    print('Temperature: {}\xb0C'.format(celcius(data['currently']['temperature'])))
+    print()
+    print('Location :', geo['formatted_address'])
+    print('Weather  :', data['currently']['summary'])
+    print('Currently: {}\xb0C'.format(int(data['currently']['temperature'])))
+    print('Forecast :', data['daily']['summary'])
 
 
 if __name__ == '__main__':
